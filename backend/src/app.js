2fb33e8308fe702024a5c847bab318e5431b737b
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2025-08-26 22:42:16 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,15 +5,29 @@ const mongoose = require('mongoose');
 const studentRoutes = require('./routes/studentRoutes');
 
 const app = express();
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://candidate-seat-allocation.netlify.app/'],
-    credentials: true
-}));
 const PORT = process.env.PORT || 5000;
 const mongo_uri = process.env.MONGO_URI;
 
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://candidate-seat-allocation.netlify.app',
+    'https://candidate-seat-allocation.onrender.com'
+];
+
 // Middleware
 app.use(express.json());
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, curl, etc.)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            return callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 // MongoDB connection
 mongoose.connect(mongo_uri)
